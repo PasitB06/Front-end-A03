abd3993c7d11381717503cf27b635b9ae565e6e3
admin <example@email.com>
2025-03-13 00:08:29 +0700

    sfadsfadsdfsdfa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-import Banner from "./components/Banner";
-import Card from "./components/Card";
+import Banner from "@/components/Banner";
+import Card from "@/components/Card";
 
 export default function Home() {
   return (
